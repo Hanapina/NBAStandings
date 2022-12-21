b974a37e3aa7a3ef359a88addaa0c96cfc41105f
Hanapina <jewli@ucdavis.edu>
2022-12-21 11:50:23 -0800

    Indentation issues with code.

--- WarriorsWebScraper/src/Scraper/Scraper.java
+++ WarriorsWebScraper/src/Scraper/Scraper.java
@@ -18,201 +18,201 @@ public class Scraper {
 	 * Current main function. May be separated out later for easier reading.
 	 * @param args
 	 */
-    public static void main(final String[] args) {
-    	List<Team> westernTeamList = new LinkedList<>();
-    	List<Team> easternTeamList = new LinkedList<>();
-    	
-    	// Western Conference Initialized
-    	System.out.println("Initializing Western Conference.\n");
-    	initializeTeam("Golden State Warriors", "https://www.basketball-reference.com/teams/GSW/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Denver Nuggets", "https://www.basketball-reference.com/teams/DEN/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Utah Jazz", "https://www.basketball-reference.com/teams/UTA/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Portland Trail Blazers", "https://www.basketball-reference.com/teams/POR/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Minnesota Timberwolves", "https://www.basketball-reference.com/teams/MIN/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Oklahoma City Thunder", "https://www.basketball-reference.com/teams/OKC/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Phoenix Suns", "https://www.basketball-reference.com/teams/PHO/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Sacramento Kings", "https://www.basketball-reference.com/teams/SAC/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Los Angeles Clippers", "https://www.basketball-reference.com/teams/LAC/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Los Angeles Lakers", "https://www.basketball-reference.com/teams/LAL/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("New Orleans Pelicans", "https://www.basketball-reference.com/teams/NOP/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Memphis Grizzlies", "https://www.basketball-reference.com/teams/MEM/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Dallas Mavericks", "https://www.basketball-reference.com/teams/DAL/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Houston Rockets", "https://www.basketball-reference.com/teams/HOU/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("San Antonio Spurs", "https://www.basketball-reference.com/teams/SAS/2023.html", westernTeamList, easternTeamList);	
-    	System.out.println("Done.");
-    	System.out.println();
-    	
-    	// Eastern Conference Initialized
-    	System.out.println("Initializing Eestern Conference.\n");
-    	initializeTeam("Boston Celtics", "https://www.basketball-reference.com/teams/BOS/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Brooklyn Nets", "https://www.basketball-reference.com/teams/BRK/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Toronto Raptors", "https://www.basketball-reference.com/teams/TOR/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Philadelphia 76ers", "https://www.basketball-reference.com/teams/PHI/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("New York Knicks", "https://www.basketball-reference.com/teams/NYK/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Milwaukee Bucks", "https://www.basketball-reference.com/teams/MIL/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Cleveland Cavaliers", "https://www.basketball-reference.com/teams/CLE/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Indiana Pacers", "https://www.basketball-reference.com/teams/IND/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Chicago Bulls", "https://www.basketball-reference.com/teams/CHI/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Detroit Pistons", "https://www.basketball-reference.com/teams/DET/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Atlanta Hawks", "https://www.basketball-reference.com/teams/ATL/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Miami Heat", "https://www.basketball-reference.com/teams/MIA/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Washington Wizards", "https://www.basketball-reference.com/teams/WAS/2023.html", westernTeamList, easternTeamList);
-    	initializeTeam("Charlotte Hornets", "https://www.basketball-reference.com/teams/CHO/2023.html", westernTeamList, easternTeamList);	
-    	initializeTeam("Orlando Magic", "https://www.basketball-reference.com/teams/ORL/2023.html", westernTeamList, easternTeamList);
-    	System.out.println("Done.");
-    	System.out.println();
-    	
-    	
-    	// Sorting each conference array by rank. 
-    	Collections.sort(westernTeamList, Comparator.comparing(Team::getHiddenNumRank));
-    	Collections.sort(easternTeamList, Comparator.comparing(Team::getHiddenNumRank));
-    	
-    	// MENU LOGIC
-    	boolean token = false;
-    	
-    	while (!token) { 
-    		System.out.println("Please choose which conference you want to look at.");
-        	System.out.println("1. Western Conference");
-        	System.out.println("2. Eastern Conference");
-        	System.out.println("3. Exit");
-        	System.out.println();
-        	String input;
-        	boolean numCheck = false;
-        	int actualNum = -1;
-        	
-        	// Sanitizing our input to make sure its an integer only. 
-        	while (!numCheck) {
-        		Scanner userInput = new Scanner(System.in);
-            	input = userInput.next();
-            	try {
-            		actualNum = Integer.parseInt(input);
-            		numCheck = true;
-            	} catch (NumberFormatException e) {
-            		System.out.println("You did not enter a number.");
-            	}
-        	}
-        	
-        	// Switch statement to handle the input.
-        	switch(actualNum) {
-	    		case 1: 
-	    			for (Team team : westernTeamList) {
-	    				printInfo(team);
-	    			}
-	    			break;
-	    		case 2:
-	    			for (Team team: easternTeamList) {
-	    				printInfo(team);
-	    			}
-	    			break;
-	    		case 3:
-	    			token = true;
-	    			System.out.println("Exiting.");
-	    			break;
-	        	default: 
-	        		System.out.println("You did not pick 1 or 2.");
-	        }
-    	}
-    }
-    
-    
-    
-    
-    
-    // **************************************************
-    // Private methods
-    // **************************************************
-    
-    /**
-     * Initializes Teams and each web page. Sorts them into the right conference.  
-     * Information is extracted from another function.
-     * @param team: The Team that is being initiated
-     * @param link: The link for each team used
-     * @param westList: a list to contain the western team
-     * @param eastList: a list to contain the eastern team
-     */
-    private static void initializeTeam(String team, String link, List<Team> westList, List<Team> eastList) {
-    	HtmlPage page = null;
-    	Team teamObj = new Team(team, "", "", "", 0);
-    	WebClient webClient = new WebClient(BrowserVersion.CHROME);
-    	
-    	// Sets of useless errors
-    	webClient.getOptions().setCssEnabled(false);
-    	webClient.getOptions().setJavaScriptEnabled(false);
-    	
-    	// Gets the current page with link.
-    	try {
-    		page = webClient.getPage(link);
-    		webClient.getCurrentWindow().getJobManager().removeAllJobs();
-    		webClient.close();
-    	} catch (IOException e) {
-    		System.out.println("An error has occured: " + e);
-    	}
-    	
-    	// Information extracted.
-    	extractInformation(teamObj, page);
-    	
-    	// Sorts into the right list for sorting later.
-    	if (teamObj.getConference().equals("Western Conference")) {
-    		westList.add(teamObj);
-    	} else {
-    		eastList.add(teamObj);
-    	}
-    }
-    
-    
-    /**
-     * Extracts information for the Websites listed. 
-     * All extracted Websites are in the same format. 
-     * numRank seems repetitive but allows for easier sorting.
-     * @param team: The team the information is extracted into
-     * @param page: The page we are extracting information from
-     */
-    private static void extractInformation(Team team, HtmlPage page) {
-    	List<DomText> items = page.getByXPath("//p//text()");
-    	
-    	//Testing Statements for now
-    	/*
+	public static void main(final String[] args) {
+		List<Team> westernTeamList = new LinkedList<>();
+		List<Team> easternTeamList = new LinkedList<>();
+
+		// Western Conference Initialized
+		System.out.println("Initializing Western Conference.\n");
+		initializeTeam("Golden State Warriors", "https://www.basketball-reference.com/teams/GSW/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Denver Nuggets", "https://www.basketball-reference.com/teams/DEN/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Utah Jazz", "https://www.basketball-reference.com/teams/UTA/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Portland Trail Blazers", "https://www.basketball-reference.com/teams/POR/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Minnesota Timberwolves", "https://www.basketball-reference.com/teams/MIN/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Oklahoma City Thunder", "https://www.basketball-reference.com/teams/OKC/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Phoenix Suns", "https://www.basketball-reference.com/teams/PHO/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Sacramento Kings", "https://www.basketball-reference.com/teams/SAC/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Los Angeles Clippers", "https://www.basketball-reference.com/teams/LAC/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Los Angeles Lakers", "https://www.basketball-reference.com/teams/LAL/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("New Orleans Pelicans", "https://www.basketball-reference.com/teams/NOP/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Memphis Grizzlies", "https://www.basketball-reference.com/teams/MEM/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Dallas Mavericks", "https://www.basketball-reference.com/teams/DAL/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Houston Rockets", "https://www.basketball-reference.com/teams/HOU/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("San Antonio Spurs", "https://www.basketball-reference.com/teams/SAS/2023.html", westernTeamList, easternTeamList);	
+		System.out.println("Done.");
+		System.out.println();
+
+		// Eastern Conference Initialized
+		System.out.println("Initializing Eestern Conference.\n");
+		initializeTeam("Boston Celtics", "https://www.basketball-reference.com/teams/BOS/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Brooklyn Nets", "https://www.basketball-reference.com/teams/BRK/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Toronto Raptors", "https://www.basketball-reference.com/teams/TOR/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Philadelphia 76ers", "https://www.basketball-reference.com/teams/PHI/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("New York Knicks", "https://www.basketball-reference.com/teams/NYK/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Milwaukee Bucks", "https://www.basketball-reference.com/teams/MIL/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Cleveland Cavaliers", "https://www.basketball-reference.com/teams/CLE/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Indiana Pacers", "https://www.basketball-reference.com/teams/IND/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Chicago Bulls", "https://www.basketball-reference.com/teams/CHI/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Detroit Pistons", "https://www.basketball-reference.com/teams/DET/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Atlanta Hawks", "https://www.basketball-reference.com/teams/ATL/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Miami Heat", "https://www.basketball-reference.com/teams/MIA/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Washington Wizards", "https://www.basketball-reference.com/teams/WAS/2023.html", westernTeamList, easternTeamList);
+		initializeTeam("Charlotte Hornets", "https://www.basketball-reference.com/teams/CHO/2023.html", westernTeamList, easternTeamList);	
+		initializeTeam("Orlando Magic", "https://www.basketball-reference.com/teams/ORL/2023.html", westernTeamList, easternTeamList);
+		System.out.println("Done.");
+		System.out.println();
+
+
+		// Sorting each conference array by rank. 
+		Collections.sort(westernTeamList, Comparator.comparing(Team::getHiddenNumRank));
+		Collections.sort(easternTeamList, Comparator.comparing(Team::getHiddenNumRank));
+
+		// MENU LOGIC
+		boolean token = false;
+
+		while (!token) { 
+			System.out.println("Please choose which conference you want to look at.");
+			System.out.println("1. Western Conference");
+			System.out.println("2. Eastern Conference");
+			System.out.println("3. Exit");
+			System.out.println();
+			String input;
+			boolean numCheck = false;
+			int actualNum = -1;
+
+			// Sanitizing our input to make sure its an integer only. 
+			while (!numCheck) {
+				Scanner userInput = new Scanner(System.in);
+				input = userInput.next();
+				try {
+					actualNum = Integer.parseInt(input);
+					numCheck = true;
+				} catch (NumberFormatException e) {
+					System.out.println("You did not enter a number.");
+				}
+			}
+
+			// Switch statement to handle the input.
+			switch(actualNum) {
+			case 1: 
+				for (Team team : westernTeamList) {
+					printInfo(team);
+				}
+				break;
+			case 2:
+				for (Team team: easternTeamList) {
+					printInfo(team);
+				}
+				break;
+			case 3:
+				token = true;
+				System.out.println("Exiting.");
+				break;
+			default: 
+				System.out.println("You did not pick 1 or 2.");
+			}
+		}
+	}
+
+
+
+
+
+	// **************************************************
+	// Private methods
+	// **************************************************
+
+	/**
+	 * Initializes Teams and each web page. Sorts them into the right conference.  
+	 * Information is extracted from another function.
+	 * @param team: The Team that is being initiated
+	 * @param link: The link for each team used
+	 * @param westList: a list to contain the western team
+	 * @param eastList: a list to contain the eastern team
+	 */
+	private static void initializeTeam(String team, String link, List<Team> westList, List<Team> eastList) {
+		HtmlPage page = null;
+		Team teamObj = new Team(team, "", "", "", 0);
+		WebClient webClient = new WebClient(BrowserVersion.CHROME);
+
+		// Sets of useless errors
+		webClient.getOptions().setCssEnabled(false);
+		webClient.getOptions().setJavaScriptEnabled(false);
+
+		// Gets the current page with link.
+		try {
+			page = webClient.getPage(link);
+			webClient.getCurrentWindow().getJobManager().removeAllJobs();
+			webClient.close();
+		} catch (IOException e) {
+			System.out.println("An error has occured: " + e);
+		}
+
+		// Information extracted.
+		extractInformation(teamObj, page);
+
+		// Sorts into the right list for sorting later.
+		if (teamObj.getConference().equals("Western Conference")) {
+			westList.add(teamObj);
+		} else {
+			eastList.add(teamObj);
+		}
+	}
+
+
+	/**
+	 * Extracts information for the Websites listed. 
+	 * All extracted Websites are in the same format. 
+	 * numRank seems repetitive but allows for easier sorting.
+	 * @param team: The team the information is extracted into
+	 * @param page: The page we are extracting information from
+	 */
+	private static void extractInformation(Team team, HtmlPage page) {
+		List<DomText> items = page.getByXPath("//p//text()");
+
+		//Testing Statements for now
+		/*
     	int i = 0;
     	for (DomText domText: items) {
     		System.out.println(i + ": " + domText.toString());
     		i++;
     	}
-    	*/
-    	
-    	
-    	// Extract the first line for Record and Rank
-    	String initialString = items.get(4).toString();
-    	String[] sArray = initialString.split(" ");
-    	
-    	// Grabbing the rank
-    	String rank = sArray[1];
-    	// numRank is a parsed integer to be used for easier sorting.
-    	int numRank = Integer.parseInt(sArray[1].replaceAll("[^0-9]", "")); 
-    	
-    	// Grabbing records, conferences, and the wins.
-    	String record = sArray[0].replaceAll(",", "");
-    	String conf = items.get(6).toString();
-    	String winningLine = items.get(10).toString();
-    	
-    	// Setting the Team fields
-    	team.setRank(rank);
-    	team.setRecord(record);
-    	team.setConference(conf);
-    	team.sethiddenNumRank(numRank);
-    	team.setLastGame(winningLine);
-    }
-    
-    
-    /**
-     * Basic function used to print out the information from each team.
-     * @param team: The team we are printing out.
-     */
-    private static void printInfo(Team team) {
-    	System.out.println(team.getName());
-    	System.out.println("Rank: " + team.getRank());
-    	System.out.println("Record: " + team.getRecord());
-    	System.out.println("Conference: " + team.getConference());
-    	System.out.println("Last game: " + team.getLastGame());	
-    	System.out.println();
-    }
+		 */
+
+
+		// Extract the first line for Record and Rank
+		String initialString = items.get(4).toString();
+		String[] sArray = initialString.split(" ");
+
+		// Grabbing the rank
+		String rank = sArray[1];
+		// numRank is a parsed integer to be used for easier sorting.
+		int numRank = Integer.parseInt(sArray[1].replaceAll("[^0-9]", "")); 
+
+		// Grabbing records, conferences, and the wins.
+		String record = sArray[0].replaceAll(",", "");
+		String conf = items.get(6).toString();
+		String winningLine = items.get(10).toString();
+
+		// Setting the Team fields
+		team.setRank(rank);
+		team.setRecord(record);
+		team.setConference(conf);
+		team.sethiddenNumRank(numRank);
+		team.setLastGame(winningLine);
+	}
+
+
+	/**
+	 * Basic function used to print out the information from each team.
+	 * @param team: The team we are printing out.
+	 */
+	private static void printInfo(Team team) {
+		System.out.println(team.getName());
+		System.out.println("Rank: " + team.getRank());
+		System.out.println("Record: " + team.getRecord());
+		System.out.println("Conference: " + team.getConference());
+		System.out.println("Last game: " + team.getLastGame());	
+		System.out.println();
+	}
 }
--- WarriorsWebScraper/src/Scraper/Team.java
+++ WarriorsWebScraper/src/Scraper/Team.java
@@ -1,24 +1,24 @@
 package Scraper;
 
 public class Team {
-	
+
 	// **************************************************
-    // Fields
-    // **************************************************
+	// Fields
+	// **************************************************
 	private String name;
 	private String record;
 	private String rank;
 	private String conference;
 	private String lastGame;
 	private int hiddenNumRank; // Read the Get/Set for more documentation.
-	
-	
-	
-	
-	
+
+
+
+
+
 	// **************************************************
-    // Constructors
-    // **************************************************
+	// Constructors
+	// **************************************************
 	/**
 	 * Typical constructor for teams
 	 * @param name
@@ -35,15 +35,15 @@ public class Team {
 		this.conference = conference;
 		this.hiddenNumRank = hiddenNumRank;
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 	// **************************************************
-    // Getters and Setters
-    // **************************************************
-	
+	// Getters and Setters
+	// **************************************************
+
 	/**
 	 * Gets and sets the name of the team.
 	 * @return
@@ -56,8 +56,8 @@ public class Team {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+
+
 	/**
 	 * Gets and sets the record for each team.
 	 * @return
@@ -71,7 +71,7 @@ public class Team {
 		this.record = record;
 	}
 
-	
+
 	/**
 	 * Gets and set the rank for display for each team.
 	 * @return
@@ -84,8 +84,8 @@ public class Team {
 	public void setRank(String rank) {
 		this.rank = rank;
 	}	
-	
-	
+
+
 	/**
 	 * Gets and sets the conference for each team. Can only be Western or Eastern.
 	 * Used to separate the 2 conferences for sorting later.
@@ -99,8 +99,8 @@ public class Team {
 	public void setConference(String conference) {
 		this.conference = conference;
 	}
-	
-	
+
+
 	/**
 	 * Used to sanitize and compare easier without dealing with characters.
 	 * Gets and sets the sanitized numbers for each team. Eg: 13th to 13.
@@ -110,12 +110,12 @@ public class Team {
 	public int getHiddenNumRank() {
 		return hiddenNumRank;
 	}
-	
+
 	public void sethiddenNumRank(int hiddenNumRank) {
 		this.hiddenNumRank = hiddenNumRank;
 	}
-	
-	
+
+
 	/**
 	 * Used to find the last game and the result.
 	 * Prints in (W/L) + Score + vs. 'Team'
