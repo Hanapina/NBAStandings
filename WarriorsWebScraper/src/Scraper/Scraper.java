48a8ebf28682a8602d5598f3dc8bfc952b311fab
Hanapina <jewli@ucdavis.edu>
2022-12-27 06:54:54 -0800

    Changed initializeTeam logic for better clarity + fixed formatting

--- WarriorsWebScraper/src/Scraper/Scraper.java
+++ WarriorsWebScraper/src/Scraper/Scraper.java
@@ -23,26 +23,24 @@ public class Scraper {
 	public static void main(final String[] args) throws InterruptedException {
 		List<Team> westernTeamList = new LinkedList<>();
 		List<Team> easternTeamList = new LinkedList<>();
-		
+
 		// Western Conference Initialized
-		System.out.println("Initializing Western Conference.\n");
-		initializeTeam("Golden State Warriors", "https://www.basketball-reference.com/teams/GSW/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Denver Nuggets", "https://www.basketball-reference.com/teams/DEN/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Utah Jazz", "https://www.basketball-reference.com/teams/UTA/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Portland Trail Blazers", "https://www.basketball-reference.com/teams/POR/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Minnesota Timberwolves", "https://www.basketball-reference.com/teams/MIN/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Oklahoma City Thunder", "https://www.basketball-reference.com/teams/OKC/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Phoenix Suns", "https://www.basketball-reference.com/teams/PHO/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Sacramento Kings", "https://www.basketball-reference.com/teams/SAC/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Los Angeles Clippers", "https://www.basketball-reference.com/teams/LAC/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Los Angeles Lakers", "https://www.basketball-reference.com/teams/LAL/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("New Orleans Pelicans", "https://www.basketball-reference.com/teams/NOP/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Memphis Grizzlies", "https://www.basketball-reference.com/teams/MEM/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Dallas Mavericks", "https://www.basketball-reference.com/teams/DAL/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Houston Rockets", "https://www.basketball-reference.com/teams/HOU/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("San Antonio Spurs", "https://www.basketball-reference.com/teams/SAS/2023.html", westernTeamList, easternTeamList);	
+		System.out.println("Initializing Western Conference.");
+		String[] westernTeamArray = {"Golden State Warriors", "Denver Nuggets", "Utah Jazz", "Portland Trail Blazers", "Minnesota Timberwolves",
+				"Oklahoma City Thunder", "Phoenix Suns", "Sacramento Kings", "Los Angeles Clippers", "Los Angeles Lakers", "New Orleans Pelicans",
+				"Memphis Grizzlies", "Dallas Mavericks", "Houston Rockets", "San Antonio Spurs"};
+		String[] westernTeamSites = {"https://www.basketball-reference.com/teams/GSW/2023.html", "https://www.basketball-reference.com/teams/DEN/2023.html",
+				"https://www.basketball-reference.com/teams/UTA/2023.html", "https://www.basketball-reference.com/teams/POR/2023.html", "https://www.basketball-reference.com/teams/MIN/2023.html",
+				"https://www.basketball-reference.com/teams/OKC/2023.html", "https://www.basketball-reference.com/teams/PHO/2023.html", "https://www.basketball-reference.com/teams/SAC/2023.html", 
+				"https://www.basketball-reference.com/teams/LAC/2023.html", "https://www.basketball-reference.com/teams/LAL/2023.html", "https://www.basketball-reference.com/teams/NOP/2023.html", 
+				"https://www.basketball-reference.com/teams/MEM/2023.html", "https://www.basketball-reference.com/teams/DAL/2023.html", "https://www.basketball-reference.com/teams/HOU/2023.html", 
+		"https://www.basketball-reference.com/teams/SAS/2023.html"};
+		for (int i = 0, j = 0; i < westernTeamArray.length; i++) {
+			initializeTeam(westernTeamArray[i], westernTeamSites[j++], westernTeamList, easternTeamList);
+		}
 		System.out.println("Done.");
 		System.out.println();
+
 		
 		// Added a sleep to avoid rate limiting
 		System.out.println("Sleeping 1 minute to avoid rate limits.");
@@ -50,34 +48,32 @@ public class Scraper {
 		System.out.println("Done.");
 		System.out.println();
 
+		
 		// Eastern Conference Initialized
 		System.out.println("Initializing Eestern Conference.\n");
-		initializeTeam("Boston Celtics", "https://www.basketball-reference.com/teams/BOS/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Brooklyn Nets", "https://www.basketball-reference.com/teams/BRK/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Toronto Raptors", "https://www.basketball-reference.com/teams/TOR/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Philadelphia 76ers", "https://www.basketball-reference.com/teams/PHI/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("New York Knicks", "https://www.basketball-reference.com/teams/NYK/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Milwaukee Bucks", "https://www.basketball-reference.com/teams/MIL/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Cleveland Cavaliers", "https://www.basketball-reference.com/teams/CLE/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Indiana Pacers", "https://www.basketball-reference.com/teams/IND/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Chicago Bulls", "https://www.basketball-reference.com/teams/CHI/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Detroit Pistons", "https://www.basketball-reference.com/teams/DET/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Atlanta Hawks", "https://www.basketball-reference.com/teams/ATL/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Miami Heat", "https://www.basketball-reference.com/teams/MIA/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Washington Wizards", "https://www.basketball-reference.com/teams/WAS/2023.html", westernTeamList, easternTeamList);
-		initializeTeam("Charlotte Hornets", "https://www.basketball-reference.com/teams/CHO/2023.html", westernTeamList, easternTeamList);	
-		initializeTeam("Orlando Magic", "https://www.basketball-reference.com/teams/ORL/2023.html", westernTeamList, easternTeamList);
+		String[] easternTeamArray = {"Boston Celtics", "Brooklyn Nets", "Toronto Raptors", "Philadelphia 76ers", "New York Knicks",
+				"Milwaukee Bucks", "Cleveland Cavaliers", "Indiana Pacers", "Chicago Bulls", "Detroit Pistons", "Atlanta Hawks",
+				"Miami Heat", "Washington Wizards", "Charlotte Hornets", "Orlando Magic"};
+		String[] easternTeamSites = {"https://www.basketball-reference.com/teams/BOS/2023.html", "https://www.basketball-reference.com/teams/BRK/2023.html",
+				"https://www.basketball-reference.com/teams/TOR/2023.html", "https://www.basketball-reference.com/teams/PHI/2023.html", "https://www.basketball-reference.com/teams/NYK/2023.html",
+				"https://www.basketball-reference.com/teams/MIL/2023.html", "https://www.basketball-reference.com/teams/CLE/2023.html", "https://www.basketball-reference.com/teams/IND/2023.html", 
+				"https://www.basketball-reference.com/teams/CHI/2023.html", "https://www.basketball-reference.com/teams/DET/2023.html", "https://www.basketball-reference.com/teams/ATL/2023.html", 
+				"https://www.basketball-reference.com/teams/MIA/2023.html", "https://www.basketball-reference.com/teams/WAS/2023.html", "https://www.basketball-reference.com/teams/CHO/2023.html", 
+		"https://www.basketball-reference.com/teams/ORL/2023.html"};
+		for (int i = 0, j = 0; i < easternTeamArray.length; i++) {
+			initializeTeam(easternTeamArray[i], easternTeamSites[j++], westernTeamList, easternTeamList);
+		}
 		System.out.println("Done.");
 		System.out.println();
 
-
+		
 		// Sorting each conference array by rank. 
 		Collections.sort(westernTeamList, Comparator.comparing(Team::getHiddenNumRank));
 		Collections.sort(easternTeamList, Comparator.comparing(Team::getHiddenNumRank));
 
+		
 		// MENU LOGIC
 		boolean token = false;
-
 		while (!token) { 
 			System.out.println("Please choose which conference you want to look at.");
 			System.out.println("1. Western Conference");
